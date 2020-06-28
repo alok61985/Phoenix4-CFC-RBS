6b87f72f0f59424522cadf832137bcb0a68aae5a
alok61985 <66723715+alok61985@users.noreply.github.com>
2020-06-28 21:50:15 +0530

    Add files via upload

---
+++ docs/initiateNgoBid.js
@@ -0,0 +1,110 @@
+/**
+ * Web application
+ */
+const apiUrl = 'https://04ff9d6e.eu-gb.apigw.appdomain.cloud/phoenix4ngoapp';
+
+/*const bidDetails = {
+  // retrieve the existing Admin Bid details entries
+  get() {
+    return $.ajax({
+      type: 'GET',
+      url: `${apiUrl}/adminbiddetailsread`,
+      dataType: 'json'
+    });
+  },
+  const signup={};
+  console.log("signup");*/
+  // add a single bid detail entry
+  /*add(bidName,bidId,bidRegion,bidAmount) {
+    
+    return $.ajax({
+      type: 'PUT',
+      url: `${apiUrl}/adminbidentry`,
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify({
+        bidName,
+		bidId,
+		bidRegion,
+		bidAmount,
+      }),
+      dataType: 'json',
+    });
+  }*/
+ 
+//NGO Initiated Bid entry in table
+
+const ngoInitiateBid={
+  add(bidId,costPerTree,name) {
+    
+    return $.ajax({
+      type: 'PUT',
+      url: `${apiUrl}/ngospecificbidentry`,
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify({
+        bidId,
+		costPerTree,
+		name,		
+      }),
+      dataType: 'json',
+    });
+  }
+};
+//console.log("ngoInitiateBid");
+
+(function() {
+  
+  let entriesTemplate;
+
+  function prepareTemplates() {
+    entriesTemplate = Handlebars.compile($('#entries-template').html());
+  }
+
+  // retrieve entries and update the UI
+  function loadEntries() {
+    console.log('Loading entries...');
+    $('#entries').html('Loading entries...');
+    bidDetails.get().done(function(result) {
+      if (!result.entries) {
+        return;
+      }
+
+      const context = {
+        entries: result.entries
+      }
+      $('#entries').html(entriesTemplate(context));
+    }).error(function(error) {
+      $('#entries').html('No entries');
+      console.log(error);
+    });
+  }
+ 
+
+  // intercept the click on the submit button, add the bidDetails entry and
+  // reload entries on success
+ 
+  
+  
+  //NGO Specific BID entry
+  
+  $(document).on('submit', '#initiateBid', function(e) {
+    e.preventDefault();
+
+    ngoInitiateBid.add(
+      $('#bidId').val().trim(),      
+      $('#costPerTree').val().trim(),	 
+	  $('#name').val().trim()
+    ).done(function(result) {
+      // reload entries
+      loadEntries();
+    }).error(function(error) {
+      console.log(error);
+    });
+  });
+  
+  
+  
+  $(document).ready(function() { 
+    prepareTemplates();
+    loadEntries();
+  });
+})();
---
+++ docs/signup.js
@@ -0,0 +1,105 @@
+/**
+ * Web application
+ */
+const apiUrl = 'https://04ff9d6e.eu-gb.apigw.appdomain.cloud/phoenix4ngoapp';
+
+/*const bidDetails = {
+  // retrieve the existing Admin Bid details entries
+  get() {
+    return $.ajax({
+      type: 'GET',
+      url: `${apiUrl}/adminbiddetailsread`,
+      dataType: 'json'
+    });
+  },
+  const signup={};
+  console.log("signup");*/
+  // add a single bid detail entry
+  /*add(bidName,bidId,bidRegion,bidAmount) {
+    
+    return $.ajax({
+      type: 'PUT',
+      url: `${apiUrl}/adminbidentry`,
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify({
+        bidName,
+		bidId,
+		bidRegion,
+		bidAmount,
+      }),
+      dataType: 'json',
+    });
+  }*/
+  const signup={
+  add(name,password,email) {
+    
+    return $.ajax({
+      type: 'PUT',
+      url: `${apiUrl}/ngosignup`,
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify({
+        name,
+		password,
+		email,		
+      }),
+      dataType: 'json',
+    });
+  }
+};
+
+(function() {
+  
+  let entriesTemplate;
+
+  function prepareTemplates() {
+    entriesTemplate = Handlebars.compile($('#entries-template').html());
+  }
+
+  // retrieve entries and update the UI
+  function loadEntries() {
+    console.log('Loading entries...');
+    $('#entries').html('Loading entries...');
+    bidDetails.get().done(function(result) {
+      if (!result.entries) {
+        return;
+      }
+
+      const context = {
+        entries: result.entries
+      }
+      $('#entries').html(entriesTemplate(context));
+    }).error(function(error) {
+      $('#entries').html('No entries');
+      console.log(error);
+    });
+  }
+ 
+
+  // intercept the click on the submit button, add the bidDetails entry and
+  // reload entries on success
+ 
+  //Sign Up
+  $(document).on('submit', '#signUp', function(e) {
+    e.preventDefault();
+
+    signup.add(
+      $('#name').val().trim(),      
+      $('#password').val().trim(),	 
+	  $('#email').val().trim()
+    ).done(function(result) {
+      // reload entries
+      loadEntries();
+    }).error(function(error) {
+      console.log(error);
+    });
+  });
+  
+ 
+  
+  
+  
+  $(document).ready(function() { 
+    prepareTemplates();
+    loadEntries();
+  });
+})();
